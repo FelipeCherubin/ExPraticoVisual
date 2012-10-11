a48cc55f573342bccf1ee26e95836800670817e0
Felipe Cherubin <felipe_cherubin@hotmail.com>
2012-10-11 19:43:29 -0300

    alterando texbox e labels

--- ExerPrat-Filmes/Form1.Designer.cs
+++ ExerPrat-Filmes/Form1.Designer.cs
@@ -80,6 +80,9 @@
             this.checkdata = new System.Windows.Forms.CheckBox();
             this.pesqgenero = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // listView1
@@ -117,9 +120,9 @@
             listViewGroup7,
             listViewGroup8,
             listViewGroup9});
-            this.listView1.Location = new System.Drawing.Point(10, 202);
+            this.listView1.Location = new System.Drawing.Point(10, 230);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(391, 218);
+            this.listView1.Size = new System.Drawing.Size(422, 218);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -145,7 +148,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(7, 32);
+            this.label1.Location = new System.Drawing.Point(13, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(82, 13);
             this.label1.TabIndex = 1;
@@ -155,7 +158,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(180, 69);
+            this.label2.Location = new System.Drawing.Point(186, 56);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(42, 13);
             this.label2.TabIndex = 2;
@@ -165,7 +168,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(7, 69);
+            this.label3.Location = new System.Drawing.Point(13, 56);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(108, 13);
             this.label3.TabIndex = 3;
@@ -175,7 +178,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(7, 108);
+            this.label4.Location = new System.Drawing.Point(13, 95);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(33, 13);
             this.label4.TabIndex = 4;
@@ -183,7 +186,7 @@
             // 
             // nomefilme
             // 
-            this.nomefilme.Location = new System.Drawing.Point(10, 47);
+            this.nomefilme.Location = new System.Drawing.Point(16, 34);
             this.nomefilme.Name = "nomefilme";
             this.nomefilme.Size = new System.Drawing.Size(391, 20);
             this.nomefilme.TabIndex = 5;
@@ -202,30 +205,30 @@
             "Infantil",
             "Romance",
             "Ficção-Científica"});
-            this.comboBox1.Location = new System.Drawing.Point(183, 83);
+            this.comboBox1.Location = new System.Drawing.Point(189, 70);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(114, 21);
             this.comboBox1.TabIndex = 6;
             // 
             // local
             // 
-            this.local.Location = new System.Drawing.Point(10, 124);
+            this.local.Location = new System.Drawing.Point(16, 111);
             this.local.Multiline = true;
             this.local.Name = "local";
-            this.local.Size = new System.Drawing.Size(391, 39);
+            this.local.Size = new System.Drawing.Size(391, 22);
             this.local.TabIndex = 8;
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(10, 84);
+            this.dateTimePicker1.Location = new System.Drawing.Point(16, 71);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(105, 20);
             this.dateTimePicker1.TabIndex = 9;
             // 
             // Cadastrar
             // 
-            this.Cadastrar.Location = new System.Drawing.Point(174, 169);
+            this.Cadastrar.Location = new System.Drawing.Point(205, 197);
             this.Cadastrar.Name = "Cadastrar";
             this.Cadastrar.Size = new System.Drawing.Size(65, 27);
             this.Cadastrar.TabIndex = 10;
@@ -235,7 +238,7 @@
             // 
             // Editar
             // 
-            this.Editar.Location = new System.Drawing.Point(245, 169);
+            this.Editar.Location = new System.Drawing.Point(276, 197);
             this.Editar.Name = "Editar";
             this.Editar.Size = new System.Drawing.Size(75, 27);
             this.Editar.TabIndex = 11;
@@ -245,14 +248,14 @@
             // 
             // excluir
             // 
-            this.excluir.BackColor = System.Drawing.SystemColors.Control;
+            this.excluir.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.excluir.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.excluir.Location = new System.Drawing.Point(326, 169);
+            this.excluir.Location = new System.Drawing.Point(357, 197);
             this.excluir.Name = "excluir";
             this.excluir.Size = new System.Drawing.Size(75, 27);
             this.excluir.TabIndex = 12;
             this.excluir.Text = "Excluir";
-            this.excluir.UseVisualStyleBackColor = false;
+            this.excluir.UseVisualStyleBackColor = true;
             this.excluir.Click += new System.EventHandler(this.excluir_Click);
             // 
             // textBox1
@@ -270,9 +273,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(834, 137);
+            this.button1.Location = new System.Drawing.Point(721, 197);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 38);
+            this.button1.Size = new System.Drawing.Size(75, 27);
             this.button1.TabIndex = 14;
             this.button1.Text = "Pesquisa";
             this.button1.UseVisualStyleBackColor = true;
@@ -280,9 +283,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(806, 194);
+            this.button2.Location = new System.Drawing.Point(802, 197);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(103, 30);
+            this.button2.Size = new System.Drawing.Size(103, 27);
             this.button2.TabIndex = 16;
             this.button2.Text = "Limpar Pesquisa";
             this.button2.UseVisualStyleBackColor = true;
@@ -292,7 +295,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(502, 46);
+            this.label5.Location = new System.Drawing.Point(15, 17);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(35, 13);
             this.label5.TabIndex = 17;
@@ -302,7 +305,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(502, 169);
+            this.label6.Location = new System.Drawing.Point(15, 98);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(33, 13);
             this.label6.TabIndex = 18;
@@ -312,7 +315,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(679, 86);
+            this.label7.Location = new System.Drawing.Point(142, 56);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(55, 13);
             this.label7.TabIndex = 19;
@@ -322,7 +325,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(502, 86);
+            this.label8.Location = new System.Drawing.Point(15, 57);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(59, 13);
             this.label8.TabIndex = 20;
@@ -332,7 +335,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(502, 129);
+            this.label9.Location = new System.Drawing.Point(278, 54);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(42, 13);
             this.label9.TabIndex = 21;
@@ -340,15 +343,15 @@
             // 
             // pesqnome
             // 
-            this.pesqnome.Location = new System.Drawing.Point(505, 61);
+            this.pesqnome.Location = new System.Drawing.Point(18, 32);
             this.pesqnome.Name = "pesqnome";
-            this.pesqnome.Size = new System.Drawing.Size(323, 20);
+            this.pesqnome.Size = new System.Drawing.Size(384, 20);
             this.pesqnome.TabIndex = 22;
             // 
             // dateTimePicker2
             // 
             this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker2.Location = new System.Drawing.Point(524, 102);
+            this.dateTimePicker2.Location = new System.Drawing.Point(37, 73);
             this.dateTimePicker2.Name = "dateTimePicker2";
             this.dateTimePicker2.Size = new System.Drawing.Size(102, 20);
             this.dateTimePicker2.TabIndex = 24;
@@ -356,17 +359,17 @@
             // dateTimePicker3
             // 
             this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker3.Location = new System.Drawing.Point(682, 102);
+            this.dateTimePicker3.Location = new System.Drawing.Point(145, 72);
             this.dateTimePicker3.Name = "dateTimePicker3";
             this.dateTimePicker3.Size = new System.Drawing.Size(100, 20);
             this.dateTimePicker3.TabIndex = 25;
             // 
             // pesqlocal
             // 
-            this.pesqlocal.Location = new System.Drawing.Point(505, 187);
+            this.pesqlocal.Location = new System.Drawing.Point(18, 114);
             this.pesqlocal.Multiline = true;
             this.pesqlocal.Name = "pesqlocal";
-            this.pesqlocal.Size = new System.Drawing.Size(299, 37);
+            this.pesqlocal.Size = new System.Drawing.Size(384, 19);
             this.pesqlocal.TabIndex = 26;
             // 
             // listView2
@@ -403,9 +406,9 @@
             listViewGroup16,
             listViewGroup17,
             listViewGroup18});
-            this.listView2.Location = new System.Drawing.Point(505, 230);
+            this.listView2.Location = new System.Drawing.Point(485, 230);
             this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(404, 188);
+            this.listView2.Size = new System.Drawing.Size(420, 218);
             this.listView2.TabIndex = 27;
             this.listView2.UseCompatibleStateImageBehavior = false;
             this.listView2.View = System.Windows.Forms.View.Details;
@@ -418,7 +421,7 @@
             // colunaassistido1
             // 
             this.colunaassistido1.Text = "Data";
-            this.colunaassistido1.Width = 132;
+            this.colunaassistido1.Width = 130;
             // 
             // colunalocal1
             // 
@@ -428,7 +431,7 @@
             // checkdata
             // 
             this.checkdata.AutoSize = true;
-            this.checkdata.Location = new System.Drawing.Point(505, 104);
+            this.checkdata.Location = new System.Drawing.Point(18, 75);
             this.checkdata.Name = "checkdata";
             this.checkdata.Size = new System.Drawing.Size(15, 14);
             this.checkdata.TabIndex = 29;
@@ -448,54 +451,64 @@
             "Infantil",
             "Romance",
             "Ficção-Científica"});
-            this.pesqgenero.Location = new System.Drawing.Point(505, 145);
+            this.pesqgenero.Location = new System.Drawing.Point(281, 70);
             this.pesqgenero.Name = "pesqgenero";
             this.pesqgenero.Size = new System.Drawing.Size(121, 21);
             this.pesqgenero.TabIndex = 32;
             // 
             // groupBox1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(344, 250);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.nomefilme);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.local);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 30);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.Size = new System.Drawing.Size(420, 161);
             this.groupBox1.TabIndex = 33;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Cadastro";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.pesqgenero);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.checkdata);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.pesqlocal);
+            this.groupBox2.Controls.Add(this.pesqnome);
+            this.groupBox2.Controls.Add(this.dateTimePicker3);
+            this.groupBox2.Controls.Add(this.dateTimePicker2);
+            this.groupBox2.Location = new System.Drawing.Point(485, 30);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(420, 161);
+            this.groupBox2.TabIndex = 34;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Pesquisa";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(917, 428);
+            this.ClientSize = new System.Drawing.Size(917, 452);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.pesqgenero);
-            this.Controls.Add(this.checkdata);
             this.Controls.Add(this.listView2);
-            this.Controls.Add(this.pesqlocal);
-            this.Controls.Add(this.dateTimePicker3);
-            this.Controls.Add(this.dateTimePicker2);
-            this.Controls.Add(this.pesqnome);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.excluir);
             this.Controls.Add(this.Editar);
             this.Controls.Add(this.Cadastrar);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.local);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.nomefilme);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.listView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
@@ -504,6 +517,10 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cadastro de Filmes";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -545,6 +562,7 @@
         private System.Windows.Forms.CheckBox checkdata;
         private System.Windows.Forms.ComboBox pesqgenero;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
